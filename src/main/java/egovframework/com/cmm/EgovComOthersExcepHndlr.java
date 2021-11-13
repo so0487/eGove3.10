a9b6e96bc3eb843b40d925b8572449613a10f868
so0487 <so04876725@gmail.com>
2021-11-13 11:53:29 +0900

    1st

---
+++ src/main/java/egovframework/com/cmm/EgovComOthersExcepHndlr.java
@@ -0,0 +1,18 @@
+package egovframework.com.cmm;
+
+import egovframework.rte.fdl.cmmn.exception.handler.ExceptionHandler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EgovComOthersExcepHndlr implements ExceptionHandler {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(EgovComOthersExcepHndlr.class);
+
+    public void occur(Exception exception, String packageName) {
+    	//log.debug(" EgovServiceExceptionHandler run...............");
+    	LOGGER.error(packageName, exception);
+    	LOGGER.error(exception.getMessage(), exception);
+    	LOGGER.debug("장애 발생원인을 메일발송을 하였습니다.");
+    }
+}
